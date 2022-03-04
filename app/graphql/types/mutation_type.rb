7fd20b86ad148c33f329019b33157cffc1a16010
uchidataku <bbb.rchp99@gmail.com>
2022-03-04 18:02:56 +0900

    Authなし

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,5 @@
 module Types
   # MutationType
   class MutationType < Types::BaseObject
-    field :sign_up, mutation: Mutations::SignUp
-    field :sign_in, mutation: Mutations::SignIn
   end
 end
--- spec/factories/accounts.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-FactoryBot.define do
-  factory :account do
-    sequence(:email) { |n| "sample_#{n}@example.com" }
-    password { 'password' }
-    username { 'サンプルユーザー' }
-  end
-end
